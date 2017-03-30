55a78e83fbf00fee5c06b9fc62608f6e9b7d974b
Rakshak R.Hegde <rakshakhegde@gmail.com>
2017-03-30 23:04:39 +0530

    String Not Null version

---
+++ sugarprefs/src/main/java/io/github/rakshakhegde/sugarprefs/obsrvprefs/ObsrvStringNotNullPref.kt
@@ -0,0 +1,21 @@
+package io.github.rakshakhegde.sugarprefs.obsrvprefs
+
+import android.content.SharedPreferences
+import io.github.rakshakhegde.sugarprefs.PreferenceHelper
+import io.github.rakshakhegde.sugarprefs.prefextensions.set
+
+/**
+ * Created by rakshakhegde on 23/02/17.
+ */
+class ObsrvStringNotNullPref @JvmOverloads constructor(
+		val key: String,
+		val defaultVal: String = "",
+		val preferences: SharedPreferences = PreferenceHelper.defaultPrefs
+) : ObsrvPref<String>(key, preferences) {
+
+	override fun get(): String = preferences.getString(key, defaultVal)
+
+	override fun set(value: String) {
+		preferences[key] = value
+	}
+}
